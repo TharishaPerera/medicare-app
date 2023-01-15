9533dd4a46536e06b983190ce1e853795682194b
Tharisha Perera <tharishaperera@gmail.com>
2023-01-15 17:55:21 +0530

    added navigation between first three intro pages

--- app/src/main/java/com/tharishaperera/medicare/FirstIntroActivity.kt
+++ app/src/main/java/com/tharishaperera/medicare/FirstIntroActivity.kt
@@ -1,11 +1,29 @@
 package com.tharishaperera.medicare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class FirstIntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_intro)
+
+        var txtNext : TextView
+//        var txtSkip : TextView
+
+        txtNext = findViewById(R.id.btn_f_intro_next)
+//        txtSkip = findViewById(R.id.btn_skip)
+
+        txtNext.setOnClickListener{
+            var intent = Intent(this, SecondIntroActivity::class.java)
+            startActivity(intent)
+        }
+
+//        txtSkip.setOnClickListener{
+//            var intent = Intent(this, SecondIntroActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
--- app/src/main/java/com/tharishaperera/medicare/ThirdIntroActivity.kt
+++ app/src/main/java/com/tharishaperera/medicare/ThirdIntroActivity.kt
@@ -1,11 +1,29 @@
 package com.tharishaperera.medicare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ThirdIntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_intro)
+
+        var txtNext : TextView
+        var txtBack : TextView
+
+        txtNext = findViewById(R.id.btn_t_intro_next)
+        txtBack = findViewById(R.id.btn_t_intro_back)
+
+//        txtNext.setOnClickListener{
+//            var intent = Intent(this, ThirdIntroActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        txtBack.setOnClickListener{
+            var intent = Intent(this, SecondIntroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
