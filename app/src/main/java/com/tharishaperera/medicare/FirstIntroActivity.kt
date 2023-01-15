14718476452d4502b407914545e3783842b9329d
Tharisha Perera <tharishaperera@gmail.com>
2023-01-15 17:05:49 +0530

    added first activity. design complete. need to work on button actions

---
+++ app/src/main/java/com/tharishaperera/medicare/FirstIntroActivity.kt
@@ -0,0 +1,11 @@
+package com.tharishaperera.medicare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstIntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_intro)
+    }
+}
